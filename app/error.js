ba85e962324f9f333bccb69043876007a773c9bc
Gemmuel <gemmueldelacruz@gmail.com>
2023-06-07 21:25:48 +0800

    bug fixes

--- app/error.js
+++ app/error.js
@@ -11,8 +11,10 @@ export default function Error({ error }) {
     }, [error]);
 
     return (
-        <div className="w-full h-full flex flex-col justify-center items-center">
-            <h2 className="text-3xl font-bold">Something went wrong!</h2>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <h2 className="text-3xl font-bold">
+                Something went wrong! Go back and refresh the page!
+            </h2>
             <button
                 className="p-4 border border-white"
                 onClick={() => router.push("/")}
--- app/not-found.js
+++ app/not-found.js
@@ -5,8 +5,10 @@ export default function notFoundPage() {
     const router = useRouter();
 
     return (
-        <div className="w-full h-full flex flex-col justify-center items-center">
-            <h2 className="text-3xl font-bold">Page not Found!</h2>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <h2 className="text-3xl font-bold">
+                Page not Found! Go back and refresh the page!
+            </h2>
             <button
                 className="p-4 border border-white"
                 onClick={() => router.push("/")}
