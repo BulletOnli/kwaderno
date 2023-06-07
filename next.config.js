127b6f349dc0f81633ad5fe591443cb63197ff49
Gemmuel <gemmueldelacruz@gmail.com>
2023-06-07 23:16:59 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        FIREBASE_API_KEY: process.env.apiKey,
+        AUTH_DOMAIN: process.env.authDomain,
+        PROJECT_ID: process.env.projectId,
+        STORAGE_BUCKET: process.env.storageBucket,
+        MESSAGING_SENDER_ID: process.env.messagingSenderId,
+        APP_ID: process.env.appId,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
