26f5ad57b7f4c7b5a74c828fc67a26bb275c4404
Gemmuel <gemmueldelacruz@gmail.com>
2023-06-09 07:46:49 +0800

    config

--- app/firebase.config.js
+++ app/firebase.config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
+    apiKey: "AIzaSyDfJARmETNk_VvJAzZ202H91Ko87-xSwr0",
+    authDomain: "kwadernote-cf746.firebaseapp.com",
+    projectId: "kwadernote-cf746",
+    storageBucket: "kwadernote-cf746.appspot.com",
+    messagingSenderId: "320182407150",
+    appId: "1:320182407150:web:52b4864c325ab9fd7e88ff",
 };
